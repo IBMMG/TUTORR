cb4cbfa6495c19d91eaef1bd7ac34cf5c9c8db10
isabell sommerfeld <isi.marie06@gmail.com>
2019-11-19 17:34:50 +0000

    navbarbookingform

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :lessons do
     resources :bookings
   end
+
+  get '/bookings', to: 'bookings#index'
+
   resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

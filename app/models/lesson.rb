a2374e143049d16b4cf5db3b08e033c917938dbc
annatytko <tytko.anna@gmail.com>
2019-11-19 09:01:03 +0000

    changed lesson_params

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,5 @@
 class Lesson < ApplicationRecord
   has_many :bookings
-
   validates :name, presence: true
   validates :price, presence: true, numericality: true
   validates :location, presence: true

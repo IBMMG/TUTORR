74126e759631d449e9315649c1724ad155724e55
annatytko <tytko.anna@gmail.com>
2019-11-21 15:29:16 +0000

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  # def show
+  #   @lesson = Lesson.find(params[:lesson_id])
+  #   @review = review.find(params[:id])
+  #   @review.lesson = @review
+  # end
+
+  def new
+    @lesson = Lesson.find(params[:lesson_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @lesson = Lesson.find(params[:lesson_id])
+    @review.lesson = @review
+
+    if @review.save
+      redirect_to lesson_path(@lesson, @review)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:comment, :name, :stars, :booking_id)
+  end
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,7 @@
 class Lesson < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   has_many :bookings
+  has_many :reviews
 
   validates :name, presence: true
   validates :price, presence: true, numericality: true
